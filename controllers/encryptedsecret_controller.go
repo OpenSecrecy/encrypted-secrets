187759fc436e51853cc1ce64759f98141a419b64
shubhindia <shubhindia123@gmail.com>
2023-08-04 16:03:41 +0530

    add manifests.yaml to deploy/manifests.yaml
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- controllers/encryptedsecret_controller.go
+++ controllers/encryptedsecret_controller.go
@@ -43,6 +43,7 @@ type EncryptedSecretReconciler struct {
 //+kubebuilder:rbac:groups=secrets.shubhindia.xyz,resources=encryptedsecrets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=secrets.shubhindia.xyz,resources=encryptedsecrets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=secrets.shubhindia.xyz,resources=encryptedsecrets/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 func (r *EncryptedSecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	r.log = log.FromContext(ctx).WithValues("EncryptedSecret", req.NamespacedName)
