f9e6d3336858708041acfb2f7d39e4583bd155c0
shubhindia <shubhindia123@gmail.com>
2023-11-03 12:37:09 +0530

    fix metricsAddrs
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ import (
 
 	secretsv1alpha1 "github.com/opensecrecy/encrypted-secrets/api/v1alpha1"
 	"github.com/opensecrecy/encrypted-secrets/controllers"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -66,9 +67,10 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: metricsAddr,
+		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "f9338bfe.opensecrecy.org",
